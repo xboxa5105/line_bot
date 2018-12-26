004be57be9a37e8ce292ff27a3b23c7a28c1bfbb
翁國筌 <wengguoquan@wengguoquande-MacBook-Pro.local>
2018-12-26 11:54:14 +0800

    Test Time

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func main() {
 	rate_data, _ := http_bank()
 	// exchange_rate_data, _ := json.Marshal(rate_data)
 	usd := fmt.Sprintf("美金 : %s\n日圓 : %s\n英鎊 : %s\n歐元 : %s", rate_data.USD, rate_data.JPY, rate_data.GBP, rate_data.EUR)
-	spec := "0 0 1,4,7 * * *"
+	spec := "0 0 * * * *"
 	c := cron.New()
 	c.AddFunc(spec, func() {
 		line_post(usd)
